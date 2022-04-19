3a21af91e9f5e05cfe741e01863f1460a36a0d71
leemoriarity <73798970+leemoriarity@users.noreply.github.com>
2022-04-18 22:31:52 -0400

    botted gui update

---
+++ target/web-1.0-SNAPSHOT/WEB-INF/classes/static/js/botted.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function($) {
+    $('.results').toggle();
+	$('.submit').click(function() {
+        $( ".results" ).css("visibility","hidden");
+        $( ".isbot" ).css("visibility","hidden");
+        $( ".data" ).css("visibility","hidden");
+        $( ".loading" ).html('<br><img src="/images/loading.gif" width="80">');
+        $( ".result" ).html('');
+    });
+
+    $('.showdata').click(function() {
+        $( ".results" ).css("visibility","visible");
+        if ($(this).val() == "show data")
+            $( ".showdata" ).val("hide data");
+        else
+            $( ".showdata" ).val("show data");
+        $('.results').toggle();
+    });
+
+   var hamburger = $("#hamburger_menu");
+   var menu = $("#menubody");
+   $(hamburger).click(function (e) {
+     menu.toggleClass("open");
+     hamburger.toggleClass("open");
+   });
+
+});
